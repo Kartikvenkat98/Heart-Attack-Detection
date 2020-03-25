7677335c754e0d71d99f177633df22cc12331724
kartik <kartikvenkat98@gmail.com>
2020-03-25 16:50:00 -0500

    Modify script to take argument from cmd

--- frame_generator.py
+++ frame_generator.py
@@ -1,20 +1,29 @@
 import cv2
+import sys
+import os
+import shutil
 
-def FrameCapture(path): 
-      
-    vidObj = cv2.VideoCapture(path) 
+def FrameCapture(path):
+
+	dir_path = "./frames/"
+
+	if os.path.exists(dir_path):
+		shutil.rmtree(dir_path)
+
+	os.mkdir(dir_path)
+	  
+	vidObj = cv2.VideoCapture(path) 
   
-    count = 0
+	count = 0
 
-    success, image = vidObj.read()
+	success, image = vidObj.read()
   
-    while success: 
+	while success: 
 
-        cv2.imwrite("./frames/frame%d.jpg" % count, image) 
-        success, image = vidObj.read()
-        count += 1
-        print(count)
+		cv2.imwrite(dir_path + "/frame%d.jpg" % count, image) 
+		success, image = vidObj.read()
+		count += 1
+		print(count)
 
-if __name__ == '__main__': 
-   
-    FrameCapture("./videos/video_1.mp4")
+
+FrameCapture(sys.argv[1])
