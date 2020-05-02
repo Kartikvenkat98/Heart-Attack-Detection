87c65c864b34d809dad3f8b6f91f19573ce567ce
kartik <kartikvenkat98@gmail.com>
2020-05-01 22:46:32 -0500

    Add facial expression recognition to detect heart attacks

--- test_modified.py
+++ test_modified.py
@@ -9,7 +9,10 @@ import re
 import json
 import sys
 from subprocess import check_output
+from keras.models import model_from_json
 
+model = model_from_json(open("./model/model_fer.json", "r").read())
+model.load_weights('./model/model_fer.h5')
 
 MODEL_PATH = "./model/" + "model.h5"    
 
@@ -22,6 +25,32 @@ CLASSES = ['00None', '01Infarct']
 print("Loading model from:", MODEL_PATH)
 NET = load_model(MODEL_PATH)
 NET.summary()
+
+def emotion_analysis(emotions):
+
+    objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    y_pos = np.arange(len(objects))
+    
+    plt.bar(y_pos, emotions, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('percentage')
+    plt.title('emotion')
+    
+    plt.show()
+    return emotions
+
+def face_analysis(file):
+
+  img = load_img(file, grayscale=True, target_size=(48, 48))
+
+  x = img_to_array(img)
+  x = np.expand_dims(x, axis = 0)
+
+  x /= 255
+
+  custom = model.predict(x)
+  #print(custom[0][5])
+  return emotion_analysis(custom[0])
 
 
 file_name = sys.argv[1]
@@ -58,10 +87,33 @@ for img in sorted(glob.glob('./fg-extract/*'), key=numericalSort):
 
 print(maxx)
 
+count = 0
+flag = False
+
 for img in sorted(glob.glob('./fg-extract/*'), key=numericalSort):
     try :
         k = predict(img)
         print(img)
+        if k >= 0.5:
+          '''if not flag:
+            emo = face_analysis(img)
+            print(emo)
+            if emo[0] >= 0.25 or emo[1] >= 0.25 or emo[2] >= 0.25 or emo[4] >= 0.25 or emo[6] >= 0.25:
+              if emo[3] <= 0.25 and emo[5] <= 0.25:
+                count += 1
+              else:
+                k = 0.0001
+            '''
+          count += 1
+            #else:
+              #k = 0.0001
+          if count >= 10:
+            flag = True
+        if flag:
+          emo = face_analysis(img)
+          print(emo)
+          if emo[0] >= 0.25 or emo[1] >= 0.25 or emo[2] >= 0.25 or emo[4] >= 0.25 or emo[6] >= 0.25:
+            k = 0.9999
         pred.append(k)
         l = re.search(r'(\d+)', img).group(0)
         t = int(l)/int(maxx) * duration
